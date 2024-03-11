5e59dac393531494df7290eb794232f78b89a120
italo manoel <32418036+italomanoel@users.noreply.github.com>
2024-03-11 12:37:57 -0300

    Create converteDado.php
    Converção de data para modo que está configurado no determinado DB.

---
+++ converteDado.php
@@ -0,0 +1,4 @@
+<?php
+/**Converte dado da requisição para 'date' para inserções no formato 'yyyy-mm-dd'*/
+date('Y-m-d',strtotime(str_replace('/','-',$REQUEST['data_para_converter'])));
+date('Y-m-d',strtotime(str_replace('/','-',$this->input->post_get('data_para_converter'))));
